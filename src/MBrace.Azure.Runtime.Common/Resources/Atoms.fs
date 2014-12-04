d23e96b43f3a5522ca1c9f5b545da04f63c2264c
krontogiannis <rodoconst@hotmail.com>
2014-12-05 00:05:15 +0200

    Implemented MBrace.Core Channel and Atom interfaces

--- src/MBrace.Azure.Runtime.Common/Resources/Atoms.fs
+++ src/MBrace.Azure.Runtime.Common/Resources/Atoms.fs
@@ -5,6 +5,8 @@ open Microsoft.WindowsAzure.Storage
 open Microsoft.WindowsAzure.Storage.Table
 open Nessos.MBrace.Azure.Runtime
 open Nessos.MBrace.Azure.Runtime.Common
+open Nessos.MBrace.Store
+open Nessos.MBrace
 
 [<AutoSerializableAttribute(true)>]
 type Atom<'T> internal (table, pk, rk, config) =
@@ -12,67 +14,73 @@ type Atom<'T> internal (table, pk, rk, config) =
     [<NonSerializedAttribute>]
     let pickler = Configuration.Serializer
 
-    member this.Delete(id: string): Async<unit> = 
-        async {
-            let! e = Table.read<FatEntity> config table pk rk
-            return! Table.delete<FatEntity> config table e
-        }
+    interface ICloudAtom<'T> with
+        member this.Id = sprintf "table/pk/rk"
+
+        member this.Dispose(): Async<unit> = 
+            async {
+                let! e = Table.read<FatEntity> config table pk rk
+                return! Table.delete<FatEntity> config table e
+            }
         
-    member this.Update(updater: 'T -> 'T, ?maxRetries : int): Async<unit> = 
-        async {
-            let interval = let r = new Random() in r.Next(2,10) 
-            let maxInterval = 5000
-            let maxRetries = defaultArg maxRetries Int32.MaxValue
-            let rec update currInterval count = async {
-                if count >= maxRetries then
-                    return raise <| exn("Maximum number of retries exceeded.")
-                else
-                    let! e = Table.read<FatEntity> config table pk rk
-                    let oldValue = pickler.UnPickle<'T>(e.GetPayload())
-                    let newValue = updater oldValue
-                    let newBinary = pickler.Pickle<'T>(newValue)
-                    let e = new FatEntity(e.PartitionKey, String.Empty, newBinary, ETag = e.ETag)
-                    let! result = Async.Catch <| Table.merge config table e
-                    match result with
-                    | Choice1Of2 _ -> return ()
-                    | Choice2Of2 e when Table.PreconditionFailed e -> 
-                        do! Async.Sleep currInterval
-                        return! update (min (interval * currInterval) maxInterval) (count+1)
-                    | Choice2Of2 e -> return raise e
+        member this.Update(updater: 'T -> 'T, ?maxRetries : int): Async<unit> = 
+            async {
+                let interval = let r = new Random() in r.Next(2,10) 
+                let maxInterval = 5000
+                let maxRetries = defaultArg maxRetries Int32.MaxValue
+                let rec update currInterval count = async {
+                    if count >= maxRetries then
+                        return raise <| exn("Maximum number of retries exceeded.")
+                    else
+                        let! e = Table.read<FatEntity> config table pk rk
+                        let oldValue = pickler.UnPickle<'T>(e.GetPayload())
+                        let newValue = updater oldValue
+                        let newBinary = pickler.Pickle<'T>(newValue)
+                        let e = new FatEntity(e.PartitionKey, String.Empty, newBinary, ETag = e.ETag)
+                        let! result = Async.Catch <| Table.merge config table e
+                        match result with
+                        | Choice1Of2 _ -> return ()
+                        | Choice2Of2 e when Table.PreconditionFailed e -> 
+                            do! Async.Sleep currInterval
+                            return! update (min (interval * currInterval) maxInterval) (count+1)
+                        | Choice2Of2 e -> return raise e
+                }
+
+                return! update interval 0
+            }       
+
+        member this.Force(newValue: 'T): Async<unit> = 
+            async {
+                let! e = Table.read<FatEntity> config table pk rk
+                let newBinary = pickler.Pickle<'T>(newValue)
+                let e = new FatEntity(e.PartitionKey, String.Empty, newBinary, ETag = "*")
+                let! _ = Table.merge config table e
+                return ()
             }
 
-            return! update interval 0
-        }       
-
-    member this.Force(newValue: 'T): Async<unit> = 
-        async {
-            let! e = Table.read<FatEntity> config table pk rk
-            let newBinary = pickler.Pickle<'T>(newValue)
-            let e = new FatEntity(e.PartitionKey, String.Empty, newBinary, ETag = "*")
-            let! _ = Table.merge config table e
-            return ()
-        }
-
-    member this.GetValue(): Async<'T> = 
-        async {
-            let! e = Table.read<FatEntity> config table pk rk
-            let value = pickler.UnPickle<'T>(e.GetPayload())
-            return value
-        }
+        member this.GetValue(): Async<'T> = 
+            async {
+                let! e = Table.read<FatEntity> config table pk rk
+                let value = pickler.UnPickle<'T>(e.GetPayload())
+                return value
+            }
 
 [<AutoSerializableAttribute(false)>]
 type AtomProvider private (table, config : ConfigurationId) =
         
-    member this.IsSupportedValue(value: 'T) : bool = 
-        Configuration.Serializer.ComputeSize(value) <= TableEntityUtils.MaxPayloadSize
+    interface ICloudAtomProvider with
+        member this.Id = sprintf "CloudAtomProvider:%s" table
+
+        member this.IsSupportedValue(value: 'T) : bool = 
+            Configuration.Serializer.ComputeSize(value) <= TableEntityUtils.MaxPayloadSize
         
-    member this.Create(initial: 'T): Async<Atom<'T>> = 
-            async {
-                let binary = Configuration.Serializer.Pickle(initial)
-                let e = new FatEntity(guid(), String.Empty, binary)
-                do! Table.insert<FatEntity> config table e
-                return new Atom<'T>(table, e.PartitionKey, e.RowKey, config)
-            }
+        member this.CreateAtom(initial: 'T) = 
+                async {
+                    let binary = Configuration.Serializer.Pickle(initial)
+                    let e = new FatEntity(guid(), String.Empty, binary)
+                    do! Table.insert<FatEntity> config table e
+                    return new Atom<'T>(table, e.PartitionKey, e.RowKey, config) :> ICloudAtom<'T>
+                }
 
     static member Create(table, config : ConfigurationId) =
         new AtomProvider(table, config)
--- src/MBrace.Azure.Runtime.Common/Resources/Channels.fs
+++ src/MBrace.Azure.Runtime.Common/Resources/Channels.fs
@@ -5,6 +5,8 @@ open System.IO
 open System.Runtime.Serialization
 open Microsoft.ServiceBus.Messaging
 open Nessos.MBrace.Azure.Runtime
+open Nessos.MBrace
+open Nessos.MBrace.Store
 open Nessos.MBrace.Azure.Runtime.Common
 
 
@@ -18,13 +20,15 @@ type SendPort<'T> internal (queueName, config : ConfigurationId) =
     [<NonSerialized>]
     let queueClient = ConfigurationRegistry.Resolve<ClientProvider>(config).QueueClient(queueName)
 
-    member __.Send(message : 'T) : Async<unit> = 
-        async {
-            let bin = Configuration.Serializer.Pickle(message)
-            use ms = new MemoryStream(bin) in ms.Position <- 0L
-            let msg = new BrokeredMessage(ms)
-            do! queueClient.SendAsync(msg)
-        }
+    interface ISendPort<'T> with
+
+        member __.Send(message : 'T) : Async<unit> = 
+            async {
+                let bin = Configuration.Serializer.Pickle(message)
+                use ms = new MemoryStream(bin) in ms.Position <- 0L
+                let msg = new BrokeredMessage(ms)
+                do! queueClient.SendAsync(msg)
+            }
 
 [<AutoSerializableAttribute(true)>]
 type ReceivePort<'T> internal (queueName, config : ConfigurationId) =
@@ -32,46 +36,52 @@ type ReceivePort<'T> internal (queueName, config : ConfigurationId) =
     [<NonSerialized>]
     let queueClient = ConfigurationRegistry.Resolve<ClientProvider>(config).QueueClient(queueName)
 
-    member __.Dispose () : Async<unit> =
-        async {
-            do! ConfigurationRegistry.Resolve<ClientProvider>(config).NamespaceClient.DeleteQueueAsync(queueName)
-        }
+    interface IReceivePort<'T> with
 
-    // TODO : Receive semantics
+        member __.Dispose () : Async<unit> =
+            async {
+                do! ConfigurationRegistry.Resolve<ClientProvider>(config).NamespaceClient.DeleteQueueAsync(queueName)
+            }
 
-    member __.Receive(?timeout : int) : Async<'T> =
-        async {
-            let timeout = 
-                match timeout with 
-                | Some timeout -> TimeSpan.FromMilliseconds(float timeout)
-                | None -> TimeSpan.MaxValue
+        // TODO : Receive semantics
 
-            let! (msg : BrokeredMessage) = queueClient.ReceiveAsync(timeout)
+        member __.Receive(?timeout : int) : Async<'T> =
+            async {
+                let timeout = 
+                    match timeout with 
+                    | Some timeout -> TimeSpan.FromMilliseconds(float timeout)
+                    | None -> TimeSpan.MaxValue
 
-            if msg <> null then 
-                try
-                    use stream = msg.GetBody<Stream>()
-                    return Configuration.Serializer.Deserialize<'T>(stream)
-                finally
-                    msg.Complete()
-            else
-                return raise <| TimeoutException()
-        }
+                let! (msg : BrokeredMessage) = queueClient.ReceiveAsync(timeout)
+
+                if msg <> null then 
+                    try
+                        use stream = msg.GetBody<Stream>()
+                        return Configuration.Serializer.Deserialize<'T>(stream)
+                    finally
+                        msg.Complete()
+                else
+                    return raise <| TimeoutException()
+            }
 
 [<AutoSerializableAttribute(false)>]
 type ChannelProvider private (config : ConfigurationId) =
     
-    member __.CreateChannel<'T> () =
-        async {
-            let queueName = guid()
-            let ns = ConfigurationRegistry.Resolve<ClientProvider>(config).NamespaceClient
-            let qd = new QueueDescription(queueName)
-            qd.EnablePartitioning <- true
-            qd.DefaultMessageTimeToLive <- MaxTTL
-            qd.LockDuration <- MaxLockDuration
-            do! ns.CreateQueueAsync(qd)
-            return new SendPort<'T>(queueName, config), new ReceivePort<'T>(queueName, config)
-        }
+    interface ICloudChannelProvider with
+        member __.Id = raise <| NotImplementedException("Id")
+
+        member __.CreateChannel<'T> () =
+            async {
+                let queueName = guid()
+                let ns = ConfigurationRegistry.Resolve<ClientProvider>(config).NamespaceClient
+                let qd = new QueueDescription(queueName)
+                qd.EnablePartitioning <- true
+                qd.DefaultMessageTimeToLive <- MaxTTL
+                qd.LockDuration <- MaxLockDuration
+                do! ns.CreateQueueAsync(qd)
+                return new SendPort<'T>(queueName, config) :> ISendPort<'T>, 
+                        new ReceivePort<'T>(queueName, config) :> IReceivePort<'T>
+            }
 
     static member Create(config : ConfigurationId) =
         new ChannelProvider(config)
