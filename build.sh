aa1ca862fad4f61bb8b4a1bd2756d801f34d4fb4
Alexander Suvorov <asuvorov-softheme@users.noreply.github.com>
2015-04-22 16:49:59 +0300

    Elliminate code duplication
    It simplify updating this script in future

--- build.sh
+++ build.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
-if [ "X$OS" = "XWindows_NT" ] ; then
+  if [ "X$OS" = "XWindows_NT" ] ; then
   # use .Net
-
-  .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  .paket/paket.exe restore -v
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
-else
-
+    RUN=""
+  else
   # use mono
-  mono .paket/paket.bootstrapper.exe
+    RUN="mono"
+  fi
+  
+  $RUN .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
 
-  mono .paket/paket.exe restore -v
+  $RUN .paket/paket.exe restore -v
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  $RUN packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
 fi
