6046ada955239e2d168ad141cd84e6f4f7529660
Alexander Suvorov <asuvorov-softheme@users.noreply.github.com>
2015-04-22 17:44:57 +0300

    fix an editor's issue
    Looks like github's wysiwyg editor broke indents. Fix it.
    Fix syntax error - orphaned "fi"

--- build.sh
+++ build.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-  if [ "X$OS" = "XWindows_NT" ] ; then
-  # use .Net
-    RUN=""
-  else
-  # use mono
-    RUN="mono"
-  fi
-  
-  $RUN .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  $RUN .paket/paket.exe restore -v
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-  $RUN packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+if [ "X$OS" = "XWindows_NT" ] ; then
+# use .Net
+  RUN=""
+else
+# use mono
+  RUN="mono"
 fi
+  
+$RUN .paket/paket.bootstrapper.exe
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+	exit $exit_code
+fi
+
+$RUN .paket/paket.exe restore -v
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+	exit $exit_code
+fi
+$RUN packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
