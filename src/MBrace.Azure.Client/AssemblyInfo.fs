c2af994deceb70a771b5712bf547bf2ff83765e5
krontogiannis <krontogiannis@outlook.com>
2015-08-17 13:52:19 +0300

    Release 0.7.0

--- src/MBrace.Azure.Client/AssemblyInfo.fs
+++ src/MBrace.Azure.Client/AssemblyInfo.fs
@@ -6,7 +6,7 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
 [<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")>]
 [<assembly: AssemblyTrademarkAttribute("MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 0.7.0, Git Hash 2f8a70, Build Date 28072015 11:55 +03:00")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 0.7.0, Git Hash 808a19, Build Date 17082015 09:26 +03:00")>]
 [<assembly: AssemblyVersionAttribute("0.7.0")>]
 [<assembly: AssemblyFileVersionAttribute("0.7.0")>]
 do ()
