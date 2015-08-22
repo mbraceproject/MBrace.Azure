5dd240fc28431c3a01f3c84a3c650f78af907a89
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2015-08-22 16:00:47 +0300

    fix test assemblies in FAKE

--- samples/MBrace.Azure.CloudService.WorkerRole/Properties/AssemblyInfo.cs
+++ samples/MBrace.Azure.CloudService.WorkerRole/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Nessos Information Technologies")]
 [assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")]
 [assembly: AssemblyTrademarkAttribute("MBrace")]
-[assembly: AssemblyMetadataAttribute("Release Signature","Version 0.10.0, Git Hash c619f6, Build Date 21082015 13:11 +03:00")]
+[assembly: AssemblyMetadataAttribute("Release Signature","Version 0.10.0, Git Hash a52394, Build Date 22082015 12:59 +03:00")]
 [assembly: AssemblyVersionAttribute("0.10.0")]
 [assembly: AssemblyFileVersionAttribute("0.10.0")]
 namespace System {
