6aa5c77266b0dddc10a0b12dc336a19353304d48
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-11-19 16:22:21 +0200

    bump version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 1.1.0
+* Refactorings and improvements to MBrace.Azure.Management.
+* Misc bugfixes.
+
 ### 1.0.0
 * 1.0 Release
 
--- src/MBrace.Azure/AssemblyInfo.fs
+++ src/MBrace.Azure/AssemblyInfo.fs
@@ -6,12 +6,12 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
 [<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")>]
 [<assembly: AssemblyTrademarkAttribute("MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.0.0, Git Hash b7f787, Build Date 16112015 11:15 +02:00")>]
-[<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.1.0, Git Hash 5558ef, Build Date 19112015 13:55 +02:00")>]
+[<assembly: AssemblyVersionAttribute("1.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0"
+    let [<Literal>] Version = "1.1.0"
 
-    let [<Literal>] ReleaseTag = "1.0.0"
+    let [<Literal>] ReleaseTag = "1.1.0"
