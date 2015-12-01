94db903ce45c7c0ce14f9573e688dd28713808f9
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2015-12-01 19:54:19 +0200

    bump version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 1.1.7
+* Add support for unicode file names in blob store.
+
 ### 1.1.6
 * Enable ServerGC by default in Worker configurations.
 
--- src/MBrace.Azure.StandaloneWorker/AssemblyInfo.fs
+++ src/MBrace.Azure.StandaloneWorker/AssemblyInfo.fs
@@ -6,12 +6,12 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
 [<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")>]
 [<assembly: AssemblyTrademarkAttribute("MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.1.6, Git Hash 8f7280, Build Date 24112015 18:30 +02:00")>]
-[<assembly: AssemblyVersionAttribute("1.1.6")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.6")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.1.7, Git Hash 133984, Build Date 01122015 17:25 +02:00")>]
+[<assembly: AssemblyVersionAttribute("1.1.7")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.1.6"
+    let [<Literal>] Version = "1.1.7"
 
-    let [<Literal>] ReleaseTag = "1.1.6"
+    let [<Literal>] ReleaseTag = "1.1.7"
