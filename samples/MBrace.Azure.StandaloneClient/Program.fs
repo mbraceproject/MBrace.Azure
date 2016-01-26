c0cd7fdd04e218c1d422903d7d4d9103fa8adab2
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-01-26 14:03:53 +0200

    add MBrace.Azure.StandaloneClient sample project

---
+++ samples/MBrace.Azure.StandaloneClient/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+open System.IO
+open MBrace.Core
+open MBrace.Azure
+
+[<EntryPoint>]
+let main argv = 
+
+    AzureWorker.LocalExecutable <- __SOURCE_DIRECTORY__ + "/../../bin/mbrace.azureworker.exe"
+
+    let config = Configuration.FromEnvironmentVariables()
+    let cluster = AzureCluster.Connect(config, logger = new ConsoleLogger())
+
+    cluster.AttachLocalWorkers(workerCount = 2)
+
+    let task = cloud { return "Hello world!" } |> cluster.Run
+        
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
