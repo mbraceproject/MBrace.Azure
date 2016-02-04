b91db57d183fba19f4f14a2a1d983846f89c38e5
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-02-04 21:02:13 +0200

    bump version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 1.1.23
+* minimize amount of calls to ServiceBus subscription management API.
+
 ### 1.1.22
 * minimize amount of calls to ServiceBus subscription management API.
 
--- src/MBrace.Azure.StandaloneWorker/AssemblyInfo.fs
+++ src/MBrace.Azure.StandaloneWorker/AssemblyInfo.fs
@@ -5,12 +5,12 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
 [<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")>]
 [<assembly: AssemblyTrademarkAttribute("MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.1.22, Git Hash 3ff376, Build Date 03022016 17:47 +02:00")>]
-[<assembly: AssemblyVersionAttribute("1.1.22")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.22")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.1.23, Git Hash 1ab139, Build Date 04022016 18:36 +02:00")>]
+[<assembly: AssemblyVersionAttribute("1.1.23")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.23")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.1.22"
+    let [<Literal>] Version = "1.1.23"
 
-    let [<Literal>] ReleaseTag = "1.1.22"
+    let [<Literal>] ReleaseTag = "1.1.23"
