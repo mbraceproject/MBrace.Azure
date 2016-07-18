043edeafd87e96c1c2405f384dcd9c901595b3f8
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-07-18 16:52:00 +0300

    bump version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 1.4.0
+* Upgrade to latest MBrace.Core.
+* Add support for Azure environment loading of Argu parameters in standalone workers.
+
 ### 1.3.0
 * Upgrade to latest MBrace.Core.
 
--- src/MBrace.Azure/AssemblyInfo.fs
+++ src/MBrace.Azure/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
 [<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")>]
 [<assembly: AssemblyTrademarkAttribute("MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.3.0, Git Hash 7780c4, Build Date 04072016 14:08 +03:00")>]
-[<assembly: AssemblyVersionAttribute("1.3.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.3.0")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.4.0, Git Hash e7118c, Build Date 18072016 13:31 +03:00")>]
+[<assembly: AssemblyVersionAttribute("1.4.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.4.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.3.0"
-    let [<Literal>] InformationalVersion = "1.3.0"
+    let [<Literal>] Version = "1.4.0"
+    let [<Literal>] InformationalVersion = "1.4.0"
 
-    let [<Literal>] ReleaseTag = "1.3.0"
+    let [<Literal>] ReleaseTag = "1.4.0"
