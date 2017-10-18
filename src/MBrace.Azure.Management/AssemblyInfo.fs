ebd73e87d1269179658146f04be8887ca67a506d
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2017-10-19 00:53:07 +0100

    fix pkg paths

--- src/MBrace.Azure.Management/AssemblyInfo.fs
+++ src/MBrace.Azure.Management/AssemblyInfo.fs
@@ -3,20 +3,20 @@ namespace System
 open System.Reflection
 
 [<assembly: AssemblyProductAttribute("MBrace.Azure")>]
-[<assembly: AssemblyCompanyAttribute("Nessos Information Technologies")>]
-[<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies.")>]
+[<assembly: AssemblyCompanyAttribute("@mbraceproject")>]
+[<assembly: AssemblyCopyrightAttribute("© Nessos Information Technologies and other contributors.")>]
 [<assembly: AssemblyTrademarkAttribute("MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.5.0, Git Hash 347260, Build Date 08062017 16:21 +01:00")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 1.5.0, Git Hash 2439dd, Build Date 18102017 23:42 +01:00")>]
 [<assembly: AssemblyVersionAttribute("1.5.0")>]
 [<assembly: AssemblyFileVersionAttribute("1.5.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "MBrace.Azure"
-    let [<Literal>] AssemblyCompany = "Nessos Information Technologies"
-    let [<Literal>] AssemblyCopyright = "© Nessos Information Technologies."
+    let [<Literal>] AssemblyCompany = "@mbraceproject"
+    let [<Literal>] AssemblyCopyright = "© Nessos Information Technologies and other contributors."
     let [<Literal>] AssemblyTrademark = "MBrace"
-    let [<Literal>] AssemblyMetadata_Release_Signature = "Version 1.5.0, Git Hash 347260, Build Date 08062017 16:21 +01:00"
+    let [<Literal>] AssemblyMetadata_Release_Signature = "Version 1.5.0, Git Hash 2439dd, Build Date 18102017 23:42 +01:00"
     let [<Literal>] AssemblyVersion = "1.5.0"
     let [<Literal>] AssemblyFileVersion = "1.5.0"
 
