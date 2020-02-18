cf00513bff0db820d445694c1928c5669643ef03
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-02-18 20:39:26 +0000

    use dotnet paket

--- build.sh
+++ build.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-if [ "X$OS" = "XWindows_NT" ] ; then
-# use .Net
-  RUN=""
-else
-# use mono
-  RUN="mono"
-fi
-  
-$RUN .paket/paket.bootstrapper.exe
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-	exit $exit_code
-fi
 
-$RUN .paket/paket.exe restore -v
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-	exit $exit_code
+dotnet tool restore
+dotnet paket restore
+
+if [ "X$OS" = "XWindows_NT" ] ; then
+  # use .Net
+  packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+else
+  # use mono
+  mono packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
 fi
-$RUN packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
